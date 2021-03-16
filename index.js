ebeebb1265100cc50ed5eaa33878c60f06fe1c89
Jon Leibham <jon@booj.com>
2021-03-16 11:19:29 -0500

    updates

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const consume = './consume/';
 const fs = require('fs');
 const optDir = './opt'
 
-fs.rmdir(optDir, { recursive: true }, (err) => {
-  if (err) { throw err }
-  fs.mkdirSync(optDir)
-})
+// fs.rmdir(optDir, { recursive: true }, (err) => {
+//   if (err) { throw err }
+//   fs.mkdirSync(optDir)
+// })
 
 fs.readdir(consume, (err, files) => {
+  console.log(files)
   files.forEach(file => {
     const name = file.replace(/\.[^/.]+$/, "")
     sharp(`./consume/${file}`)
+    .webp({quality: 90, progressive: true})
     .resize(302)
-    .webp({quality: 90})
     .toFile(`${optDir}/${name}.webp`, (err, info) => { console.log(err, info) })
   });
 });
